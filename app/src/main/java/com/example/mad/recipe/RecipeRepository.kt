315cc4859b5d8f1bcd95f48b8bca3abb67dcc5b6
21wh1a0542 <21wh1a0542@bvrithyderabad.edu.in>
2024-06-14 20:28:40 +0530

    Flavours4U categories

---
+++ app/src/main/java/com/example/mad/recipe/RecipeRepository.kt
@@ -0,0 +1,4 @@
+package com.example.mad.recipe
+
+class RecipeRepository {
+}
---
+++ app/src/main/java/com/example/mad/recipe/RecipeViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.mad.recipe
+
+import androidx.lifecycle.viewModelScope
+import com.example.mad.category.Category
+import com.example.mad.category.CategoryRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class RecipeViewModel {
+    private var _categories = MutableStateFlow<List<Category>>(emptyList())
+    val categories: StateFlow<List<Category>> = _categories
+
+    init {
+        fetchCategories()
+    }
+
+    private fun fetchCategories() {
+        // Get the data from CategoriesRepository
+        viewModelScope.launch {
+            val repo = CategoryRepository()
+            val response = repo.getCategories()
+            _categories.value = response
+        }
+        // On success, change the state
+    }
+
+}
